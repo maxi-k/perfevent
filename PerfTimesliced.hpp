377d5ba11b60f0606b706b419d2146afaa44a524
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2024-04-05 12:14:30 +0200

    Use viktors perfevent header for timeslicing.

--- PerfTimesliced.hpp
+++ PerfTimesliced.hpp
@@ -1,22 +1,21 @@
 #pragma once
 #include <thread>
 #include <utility>
+#include <atomic>
 #include <chrono>
 
-#include "PerfEvent.hpp"
+#include "PerfEventViktor.hpp"
 
 struct TimeslicedPerfBlock {
     PerfEvent& instance;
-    BenchmarkParameters params;
     std::chrono::milliseconds sleep_ms;
     size_t scale{1};
-    bool run{true}, header{true};
+    std::atomic<bool> run{true};
     // initialized last
     std::thread measurement_thread;
 
-    TimeslicedPerfBlock(PerfEvent& instance, BenchmarkParameters params, std::chrono::milliseconds sleep_ms, size_t scale = 1)
+    TimeslicedPerfBlock(PerfEvent& instance, std::chrono::milliseconds sleep_ms, size_t scale = 1)
         : instance(instance)
-        , params(std::move(params))
         , sleep_ms(sleep_ms)
         , scale(scale)
         , measurement_thread([this]() { this->measure_loop(); }) {}
@@ -30,12 +29,10 @@ struct TimeslicedPerfBlock {
         this->scale = scale;
     }
 
-    BenchmarkParameters& getParams() { return params; }
-
 private:
     void measure_loop() {
         while (run) {
-            PerfEventBlock block(instance, scale, params, std::exchange(header, false));
+            PerfEventBlock block(instance, scale);
             std::this_thread::sleep_for(sleep_ms);
         }
     }
