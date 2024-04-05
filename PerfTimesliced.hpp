b69674cb7564f0a559ab70ab9a43c0da47a6498b
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2024-04-05 12:41:01 +0200

    Add timeslice callback functionality.

--- PerfTimesliced.hpp
+++ PerfTimesliced.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <functional>
 #include <thread>
 #include <utility>
 #include <atomic>
@@ -11,13 +12,21 @@ struct TimeslicedPerfBlock {
     std::chrono::milliseconds sleep_ms;
     size_t scale{1};
     std::atomic<bool> run{true};
+    // callbacks for block start and end (e.g., to set additional params)
+    // should obvisouly be vewry fast
+    using callback_t = std::function<void(PerfEvent&)>;
+    callback_t begin_cb, end_cb;
     // initialized last
     std::thread measurement_thread;
 
-    TimeslicedPerfBlock(PerfEvent& instance, std::chrono::milliseconds sleep_ms, size_t scale = 1)
+    static inline callback_t default_cb = [](PerfEvent& instance) {};
+    TimeslicedPerfBlock(PerfEvent& instance, std::chrono::milliseconds sleep_ms, size_t scale = 1,
+                        callback_t begin_cb = default_cb, callback_t end_cb = default_cb)
         : instance(instance)
         , sleep_ms(sleep_ms)
         , scale(scale)
+        , begin_cb(std::move(begin_cb))
+        , end_cb(std::move(end_cb))
         , measurement_thread([this]() { this->measure_loop(); }) {}
 
     ~TimeslicedPerfBlock() {
@@ -33,7 +42,9 @@ private:
     void measure_loop() {
         while (run) {
             PerfEventBlock block(instance, scale);
+            begin_cb(block.e);
             std::this_thread::sleep_for(sleep_ms);
+            end_cb(block.e);
         }
     }
 };
